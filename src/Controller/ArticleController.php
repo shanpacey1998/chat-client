10c98c1c05eb9b74d436689361c5e8ae6f96cb17
Shannon Pacey <Shannon.Pacey@wrenkitchens.com>
2020-10-15 11:32:54 +0100

    new changes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,12 +3,9 @@
 
 namespace App\Controller;
 
-
-use ContainerAbbxA0H\getTemplateControllerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Template;
 
 class ArticleController extends AbstractController
 {
