d549c991452e225fc5d2f717b942ac7f3c8427e5
Shannon Pacey <Shannon.Pacey@wrenkitchens.com>
2020-10-22 14:21:22 +0100

    Added url auth redirects

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use App\Form\UserFormType;
 use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -34,6 +35,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER'))
+        {
+            return new RedirectResponse('/public/index.php/home');
+        }
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
@@ -49,40 +54,47 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/register", name="user_register")
-     *
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
      * @param GuardAuthenticatorHandler $guardHandler
-     * @param LoginFormAuthenticator $formAuthenticator
+     * @param LoginFormAuthenticator $authenticator
      * @return Response
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
     {
-        if ($request->isMethod('POST')) {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
 
-            $user = new User();
-            $user->setEmail($request->request->get('email'));
-            $user->setUsername($request->request->get('username'));
-            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
 
-            $form = $this->createForm(RegistrationFormType::class, $user);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
 
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                // $form->getData() holds the submitted values
-                // but, the original `$task` variable has also been updated
-                $user = $form->getData();
-
-                $em = $this->getDoctrine()->getManager();
-
-                $em->persist($user);
-                $em->flush();
-
-                return $guardHandler->authenticateUserAndHandleSuccess($user, $request, $formAuthenticator, 'main');
-            }
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
         }
 
-            return $this->render('user/register.html.twig', ['form' => $form->createView(), ]);
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER'))
+        {
+            return new RedirectResponse('/public/index.php/home');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -17,8 +18,20 @@ class UserController extends AbstractController
      */
     public function homepage()
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
         return $this->render('user/homepage.html.twig', [
             'title' => 'Homepage'
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="user_profile")
+     */
+    public function profile()
+    {
+        return $this->render('user/profile.html.twig', [
+            'title' => 'Profile'
         ]);
     }
 }
